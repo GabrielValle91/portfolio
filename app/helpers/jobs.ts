00a98df00cab16a55c0326febc022a828b55d9d9
Gabriel Valle <gvalle91@gmail.com>
2023-07-18 13:26:06 -0500

    create jobs file

---
+++ app/helpers/jobs.ts
@@ -0,0 +1,62 @@
+export interface IJob {
+  title: string;
+  company: string;
+  companyUrl: string;
+  synopsis: string;
+  skills: string[];
+  startDate: string;
+  endDate: string;
+}
+
+export const jobs: IJob[] = [
+  {
+    title: 'Data Engineer Consultant',
+    company: 'AIT Worldwide / Spur Freight',
+    companyUrl: 'www.aitworldwide.com',
+    synopsis:
+      'I did some really cool stuff here that helped provide some amazing insights into the various departments of the company in order for the company to be acquired by AIT Worldwide and continue to thrive by implementing improvements in order to further increase profitability and productivity',
+    skills: ['skill 1', 'skill 2'],
+    startDate: 'Mar 2021',
+    endDate: 'Dec 2022',
+  },
+  {
+    title: 'Data Engineer Consultant',
+    company: 'AIT Worldwide / Spur Freight',
+    companyUrl: 'www.aitworldwide.com',
+    synopsis:
+      'I did some really cool stuff here that helped provide some amazing insights into the various departments of the company in order for the company to be acquired by AIT Worldwide and continue to thrive by implementing improvements in order to further increase profitability and productivity',
+    skills: ['skill 1', 'skill 2'],
+    startDate: 'Mar 2021',
+    endDate: 'Dec 2022',
+  },
+  {
+    title: 'Data Engineer Consultant',
+    company: 'AIT Worldwide / Spur Freight',
+    companyUrl: 'www.aitworldwide.com',
+    synopsis:
+      'I did some really cool stuff here that helped provide some amazing insights into the various departments of the company in order for the company to be acquired by AIT Worldwide and continue to thrive by implementing improvements in order to further increase profitability and productivity',
+    skills: ['skill 1', 'skill 2'],
+    startDate: 'Mar 2021',
+    endDate: 'Dec 2022',
+  },
+  {
+    title: 'Data Engineer Consultant',
+    company: 'AIT Worldwide / Spur Freight',
+    companyUrl: 'www.aitworldwide.com',
+    synopsis:
+      'I did some really cool stuff here that helped provide some amazing insights into the various departments of the company in order for the company to be acquired by AIT Worldwide and continue to thrive by implementing improvements in order to further increase profitability and productivity',
+    skills: ['skill 1', 'skill 2'],
+    startDate: 'Mar 2021',
+    endDate: 'Dec 2022',
+  },
+  {
+    title: 'Data Engineer Consultant',
+    company: 'AIT Worldwide / Spur Freight',
+    companyUrl: 'www.aitworldwide.com',
+    synopsis:
+      'I did some really cool stuff here that helped provide some amazing insights into the various departments of the company in order for the company to be acquired by AIT Worldwide and continue to thrive by implementing improvements in order to further increase profitability and productivity',
+    skills: ['skill 1', 'skill 2'],
+    startDate: 'Mar 2021',
+    endDate: 'Dec 2022',
+  },
+];
