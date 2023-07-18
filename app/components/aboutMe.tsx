b93d96ace4e299ab82c928ed626de09a9af6f137
Gabriel Valle <gvalle91@gmail.com>
2023-07-18 13:25:51 -0500

    create about me section

---
+++ app/components/aboutMe.tsx
@@ -0,0 +1,12 @@
+const AboutMe = () => {
+  return (
+    <div>
+      About me section! here goes random length sentences that give a little
+      background on me and how I got into software engineering and data
+      analytics and how I use my educational background to enhance my abilities
+      in a possible work environment
+    </div>
+  );
+};
+
+export default AboutMe;
