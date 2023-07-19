e6fd9a40a20c68ef3e357783455f405137ff6e90
Gabriel Valle <gvalle91@gmail.com>
2023-07-19 14:26:35 -0500

    update about me section

--- app/components/aboutMe.tsx
+++ app/components/aboutMe.tsx
@@ -1,10 +1,14 @@
 const AboutMe = () => {
   return (
     <div>
-      About me section! here goes random length sentences that give a little
-      background on me and how I got into software engineering and data
-      analytics and how I use my educational background to enhance my abilities
-      in a possible work environment
+      Data Analyst and Software Engineer with a passion for creating innovative
+      solutions that improve work environments. While using using Excel, Power
+      Bi, and a background in Mathematics to provide insights into departmental
+      performance, I expanded my toolkit to software development, including Ruby
+      on Rails, JavaScript, React, and NodeJS, when I recognized a need for
+      effective software at my workplace. I bring strong skills in problem
+      solving and analytics that help companies exceed goals while cutting
+      costs. I’m a lifelong learner and avid gamer.
     </div>
   );
 };
