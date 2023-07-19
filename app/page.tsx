ccf2873e6f0f60671b924fd453e8c4c9c5fdb2ba
Gabriel Valle <gvalle91@gmail.com>
2023-07-19 13:42:05 -0500

    add sections for scrolling

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,16 @@ import { jobs, IJob } from './helpers/jobs';
 export default function Home() {
   return (
     <main className='pt-24 lg:w-1/2 lg:py-24'>
-      <section className='mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24'>
+      <section
+        id='about'
+        className='mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24'
+      >
         <AboutMe />
       </section>
-      <section className='mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24'>
+      <section
+        id='experience'
+        className='mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24'
+      >
         {jobs.map((job: IJob) => {
           return <JobListing {...job} />;
         })}
