1e0472f9ee3f83efd934bb080a92eb5eae67bacf
Gabriel Valle <gvalle91@gmail.com>
2023-07-19 14:48:55 -0500

    styling updates

--- app/components/aboutMe.tsx
+++ app/components/aboutMe.tsx
@@ -1,15 +1,22 @@
 const AboutMe = () => {
   return (
-    <div>
-      Data Analyst and Software Engineer with a passion for creating innovative
-      solutions that improve work environments. While using using Excel, Power
-      Bi, and a background in Mathematics to provide insights into departmental
-      performance, I expanded my toolkit to software development, including Ruby
-      on Rails, JavaScript, React, and NodeJS, when I recognized a need for
-      effective software at my workplace. I bring strong skills in problem
-      solving and analytics that help companies exceed goals while cutting
-      costs. I’m a lifelong learner and avid gamer.
-    </div>
+    <>
+      <div className='sticky top-0 z-20 -mx-6 mb-4 w-screen bg-slate-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0'>
+        <h2 className='text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only'>
+          About
+        </h2>
+      </div>
+      <div>
+        Data Analyst and Software Engineer with a passion for creating
+        innovative solutions that improve work environments. While using using
+        Excel, Power Bi, and a background in Mathematics to provide insights
+        into departmental performance, I expanded my toolkit to software
+        development, including Ruby on Rails, JavaScript, React, and NodeJS,
+        when I recognized a need for effective software at my workplace. I bring
+        strong skills in problem solving and analytics that help companies
+        exceed goals while cutting costs. I’m a lifelong learner and avid gamer.
+      </div>
+    </>
   );
 };
 
