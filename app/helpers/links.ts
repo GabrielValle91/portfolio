acce015de436d8ef34dc5f6373c3c35f2c2c4a00
Gabriel Valle <gvalle91@gmail.com>
2023-07-19 13:41:12 -0500

    add links helper

---
+++ app/helpers/links.ts
@@ -0,0 +1,6 @@
+export const links = [
+  { name: 'About', href: '#about' },
+  { name: 'Experience', href: '#experience' },
+  { name: 'Projects', href: '#projects' },
+  { name: 'Articles', href: '#articles' },
+];
