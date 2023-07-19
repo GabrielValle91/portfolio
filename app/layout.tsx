55fb6229be37213f9febd401112b1ababe2a537f
Gabriel Valle <gvalle91@gmail.com>
2023-07-19 13:43:14 -0500

    update styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,8 +14,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
-      <body className='mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0'>
+    <html lang='en' className='scroll-smooth'>
+      <body className='mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0 bg-slate-900 leading-relaxed text-slate-400 antialiased selection:bg-teal-300 selection:text-teal-900'>
         <main className='lg:flex lg:justify-between lg:gap-4'>
           <Header />
           {children}
