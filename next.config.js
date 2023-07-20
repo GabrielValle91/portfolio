ea311ec11625445b1ac79a3b43f6a37d9df40e63
Gabriel Valle <gvalle91@gmail.com>
2023-07-20 01:10:18 -0500

    update image whitelist

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['i.imgur.com'],
+    domains: ['i.imgur.com', 'medium.com', 'cdn-images-1.medium.com'],
   },
 };
 
