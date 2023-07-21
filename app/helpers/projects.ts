13da104f38e1a96685897cf368196ea464d049be
Gabriel Valle <gvalle91@gmail.com>
2023-07-21 15:38:59 -0500

    remove project project links

--- app/helpers/projects.ts
+++ app/helpers/projects.ts
@@ -13,7 +13,7 @@ export const projects: IProject[] = [
   {
     title: 'Wildcard',
     projectImage: 'https://i.imgur.com/UPuHZZl.png',
-    demoUrl: 'https://wildcard-sinatra.herokuapp.com/',
+    // demoUrl: 'https://wildcard-sinatra.herokuapp.com/',
     codeUrl: 'https://github.com/GabrielValle91/sinatra-project',
     synopsis:
       'A Ruby application that utilizes the Sinatra framework. This web application provides basic functions of a Warehouse Management System. Users can track clients, unique items on a per client basis, inventory levels per item and inbound and outbound transactions, and items on each transaction, for clients. Custom CSS and HTML was used for page formatting.',
@@ -24,7 +24,7 @@ export const projects: IProject[] = [
   {
     title: 'Speed',
     projectImage: 'https://i.imgur.com/jLtCSya.jpg',
-    demoUrl: 'https://speed-tms.herokuapp.com/',
+    // demoUrl: 'https://speed-tms.herokuapp.com/',
     codeUrl: 'https://github.com/GabrielValle91/rails-project',
     synopsis:
       'A Ruby on Rails application that implements OAuth for user authentication. This web application provides basic functions of a Transportation Management System. Users can track clients, drivers, locations for pickup or delivery, and shipments. Users have the ability to assign items and drivers to shipments, providing a dispatcher the ability to track work for their drivers. Javascript and JQuery were added to provide additional user functionality. Bootstrap framework provided the CSS for this project.',
@@ -41,7 +41,7 @@ export const projects: IProject[] = [
   {
     title: 'PAWS',
     projectImage: 'https://i.imgur.com/UMz13fkm.jpg',
-    demoUrl: 'https://young-beyond-21632.herokuapp.com/',
+    // demoUrl: 'https://young-beyond-21632.herokuapp.com/',
     codeUrl:
       'https://github.com/GabrielValle91/people-assisting-wandering-strays',
     synopsis:
@@ -107,7 +107,7 @@ export const projects: IProject[] = [
   {
     title: 'Lister',
     projectImage: 'https://i.imgur.com/MumfUNw.jpg',
-    demoUrl: 'https://radiant-thicket-85951.herokuapp.com/',
+    // demoUrl: 'https://radiant-thicket-85951.herokuapp.com/',
     codeUrl: 'https://github.com/GabrielValle91/lister',
     synopsis:
       'A Rails application that uses Hotwire for quick page loading. This web application allows a user to keep track of various lists and list items for each list. List items can have varying quantity, and both lists and list items can be deleted or marked complete.',
