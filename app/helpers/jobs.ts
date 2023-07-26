da77510960a98065e2a7e00b181f2d7bbeb6d82e
Gabriel Valle <gvalle91@gmail.com>
2023-07-26 17:42:39 -0500

    resume updates

--- app/helpers/jobs.ts
+++ app/helpers/jobs.ts
@@ -14,7 +14,7 @@ export const jobs: IJob[] = [
     company: 'AIT Worldwide / Intelligent Logistics',
     companyUrl: 'https://www.aitworldwide.com/',
     synopsis:
-      'Designed and implemented custom solutions for multi-team data gathering and metric reporting, leading to a 35% improvement in data accuracy and a 60% reduction in report generation time. Developed high-impact PowerBI reports, unlocking deep insight into daily, quarterly and annual financial and operational metrics that played a role in facilitating the company’s buyout.',
+      'Innovated and deployed custom solutions for multi-team data gathering and metric reporting, leading to a 35% improvement in data accuracy and a 60% reduction in report generation time. Developed high-impact Power BI reports, unlocking insight into daily, quarterly and annual financial and operational metrics that played a role in facilitating the company’s buyout.',
     skills: ['Data Analysis', 'DAX', 'Excel', 'Power Bi', 'SQL', 'VBA'],
     startDate: '2021',
     endDate: '2022',
@@ -24,7 +24,7 @@ export const jobs: IJob[] = [
     company: 'KazooHr',
     companyUrl: 'https://www.worktango.com/',
     synopsis:
-      'Implemented an overhaul of the customer-facing platform, focusing on UX and functionality. Leveraged React and Rails, resulting in a visually appealing and highly intuitive interface. Spearheaded the seamless integration of a robust GraphQL API with the new React frontend and multiple internal company applications, significantly enhancing data retrieval efficiency. ',
+      'Enacted an overhaul of the customer-facing platform, focusing on UX and functionality, leveraging React and Rails, resulting in a visually appealing and highly intuitive interface. Spearheaded the seamless integration of a robust GraphQL API with the new React frontend and multiple internal company applications, significantly enhancing data retrieval efficiency. ',
     skills: [
       'GraphQL',
       'Javascript',
@@ -43,7 +43,7 @@ export const jobs: IJob[] = [
     company: 'Intelligent Logistics',
     companyUrl: 'www.intelligentlogistics.com',
     synopsis:
-      'Designed a comprehensive suite of dynamic reports, delivering operational and financial insights across five departments which was a catalyst for informed decision-making at every organizational level. Conceptualized and executed a load board within MS Access, custom-tailored for the Truck Brokerage department, which streamlined and optimized load management, resulting in enhanced operational efficiency.',
+      'Designed a comprehensive suite of dynamic reports, delivering operational and financial insights across five departments which was a catalyst for informed decision-making at every organizational level. Conceptualized and executed a custom-tailored load board within MS Access, which streamlined and optimized load management, refining operational efficiency by 8%.',
     skills: [
       'Data Analysis',
       'DAX',
@@ -61,7 +61,7 @@ export const jobs: IJob[] = [
     company: 'Intelligent Logistics',
     companyUrl: 'www.intelligentlogistics.com',
     synopsis:
-      'Implemented operational and financial reports that led to cost reduction and increased efficiency. Pioneered development of SPEED, a game-changing replacement for the existing TMS, which enhanced workflow and streamlined operations, resulting in increased levels of productivity and customer satisfaction.',
+      'Implemented operational and financial reports that led to 22% cost reduction and 13% increased efficiency. Pioneered development of a game-changing replacement for the existing transportation management system, which augmented workflow, streamlined operations, and elevated productivity by 23%.',
     skills: ['Data Analysis', 'Excel', 'MS Access', 'Power Bi', 'SQL', 'VBA'],
     startDate: '2017',
     endDate: '2018',
@@ -71,7 +71,7 @@ export const jobs: IJob[] = [
     company: 'Intelligent Logistics',
     companyUrl: 'www.intelligentlogistics.com',
     synopsis:
-      'Led end-to-end management of the inventory system, including order creation and monitoring of inventory levels, and client communication, resulting in streamlined operations and uninterrupted client satisfaction. Engineered a sophisticated Excel tool to execute precise billing calculations, which expedited the billing process and improved accuracy, leading to increased cost savings and heightened client trust.',
+      'Led end-to-end management of the inventory system, including order creation and monitoring of inventory levels, and client communication, producing streamlined operations and supreme client satisfaction. Architectured a sophisticated Excel tool to execute precise billing calculations, which expedited the billing process and improved accuracy, leading to a 17% cost reduction and heightened client trust.',
     skills: ['Data Analysis', 'Excel', 'VBA'],
     startDate: '2016',
     endDate: '2017',
